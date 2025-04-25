e0dbe651f105de0a5e6f7004230636c9107834f4
VarlorsInnovations <varlorsinnovations@gmail.com>
2025-04-25 17:59:10 +0200

    starting to apply the css classes

--- Backend/Pages/Search.cshtml.cs
+++ Backend/Pages/Search.cshtml.cs
@@ -14,9 +14,23 @@ public class SearchModel(ApplicationDbContext dbContext, VisitService visitServi
     [BindProperty] 
     public List<PostPreviewModel>? SearchResult { get; private set; }
 
+    [BindProperty] public List<PostPreviewModel> RecentPosts { get; private set; } = [];
+    
     public async Task<IActionResult> OnGetAsync()
     {
         await visitService.AddSiteAsync(this.HttpContext);
+        
+        RecentPosts = await dbContext.Posts
+            .Where(x => x.IsPublished)
+            .Where(x => x.CreatedAt >= DateTime.UtcNow.AddDays(-10))
+            .Select(x => new PostPreviewModel(
+                x.Id,
+                x.Title,
+                x.Summary,
+                x.UrlIdentifier,
+                x.Tags.Select(t => t.Content).ToList(),
+                x.CreatedAt))
+            .ToListAsync();
         
         if (SearchTerm is null)
         {
